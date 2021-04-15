f88143ec11ab8a02de26b9df4b121154f93dd175
TimFinnigan <tim.finnigan1@gmail.com>
2021-04-15 10:39:15 -0700

    add shuffleArray function

--- components/Grid.js
+++ components/Grid.js
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 import emoji from 'emoji-dictionary';
+import shuffleArray from './shuffle-array';
 
 const Grid = () => {
+	const [total, setTotal] = useState(6);
+
+	const init = () => {
+		// shuffle array
+		const shuffled = shuffleArray(emoji.names);
+		let arr = shuffled.slice(0, total);
+		console.log(arr);
+	};
 	useEffect(() => {
-		// console log emoji names
-		console.log(emoji.names);
+		init();
 	}, []);
 	return (
 		<View>
